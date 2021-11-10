f0340477701885577331ab922cc11d393aad49c1
gpiraja <germanopiraja@gmail.com>
2021-11-09 22:58:22 -0300

    Servidor express com morgan para paginas estaticas

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express =  require ('express');
 const http = require ('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use (express.static (__dirname+'/public'));
 
 app.use ((req, res, next) => {
     console.log(req.headers);
+    console.log ('Control+C para encerrar servidor');
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>Esse eh um servidor express!</h1></body></html>');
@@ -17,5 +23,6 @@ app.use ((req, res, next) => {
 const server = http.createServer(app);
 
 server.listen(port, hostname, () =>{
-     console.log(`Servidor rodando em http://${hostname}:${port}/`)
+     console.log(`Servidor rodando em http://${hostname}:${port}/`);
+     console.log ('Control+C para encerrar o servidor');
 });
